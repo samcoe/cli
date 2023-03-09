f1762b2e42acde5ff0202dd7cdd38e1803a06e2d
Sam Coe <samcoe@users.noreply.github.com>
2023-03-09 12:58:28 +1100

    something

--- x.c
+++ x.c
@@ -1,4 +1,7 @@
 int main() {
+    evil();
     good();
-    more_good();
+    more_evil();
+    even_more_evil();
+    //^[[0m^[[5A^[[5M     good();^[E     more_good();^[E^[[32m+    even_more_good();^[[0m
 }
